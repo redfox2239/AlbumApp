47637e866c4af0064579ac25a9ed99c7d4eef159
REO HARADA <rharada@hacktothepeople.local>
2019-02-02 16:59:23 +0900

    Initial Commit

---
+++ AlbumApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlbumApp
+//
+//  Created by REO HARADA on 2019/02/02.
+//  Copyright © 2019年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
